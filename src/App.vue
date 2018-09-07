bc82cb9e208b727bdd2bd74459a52554dfdd7c83
161250014 <161250014@smail.nju.edu.cn>
2018-09-07 09:45:23 +0800

    add header UI

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,27 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <el-container>
+    <el-header>
+      <function-pane></function-pane>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
+import functionPane from './components/functionPane'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {functionPane}
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    .el-header {
+        height: 100px !important;
+        padding: 0 !important;
+    }
 </style>
---
+++ src/components/functionPane.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="header">
+        <img src="../assets/logo.png" class="logo"/>
+        <div class="load-button-container">
+            <el-button class="load-button" circle><i class="el-icon-edit-outline load-icon"></i></el-button>
+            <el-button class="load-button" circle><i class="el-icon-download load-icon"></i></el-button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "functionPane"
+    }
+</script>
+
+<style scoped>
+    .header {
+        height: 100px;
+        position: relative;
+        background-color: #EEEEEE;
+    }
+
+    .logo {
+        margin: 10px 20px;
+        width: 100px;
+        height: 80px;
+        background-image: url(../assets/logo.png);
+    }
+
+    .load-button-container {
+        float: right;
+    }
+
+    .load-button {
+        height: 60px;
+        width: 60px;
+        margin: 20px;
+        font-size: 30px;
+    }
+</style>
