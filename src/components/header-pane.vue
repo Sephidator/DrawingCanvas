f1239c07f71294c43359c5d6fd9e51d726faffac
161250014 <161250014@smail.nju.edu.cn>
2018-09-07 18:47:21 +0800

    edit header UI

--- src/components/header-pane.vue
+++ src/components/header-pane.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="header">
         <img src="../assets/logo.png" class="logo"/>
+        <div class="title">绘画板</div>
         <div class="load-button-container">
             <el-button class="load-button" circle><i class="el-icon-edit-outline load-icon"></i></el-button>
             <el-button class="load-button" circle><i class="el-icon-download load-icon"></i></el-button>
@@ -17,19 +18,26 @@
 <style scoped>
     .header {
         height: 100px;
-        position: relative;
         background-color: #43B0FE;
+        display: flex;
     }
 
     .logo {
         margin: 10px 20px;
+        float: left;
         width: 100px;
         height: 80px;
         background-image: url(../assets/logo.png);
     }
 
+    .title {
+        font-size: 48px;
+        color: white;
+        margin: auto auto auto 20px;
+    }
+
     .load-button-container {
-        float: right;
+        margin-right: 0;
     }
 
     .load-button {
